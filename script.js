bf891e26e7774ebf6375e1c6a59d1751a68eedd2
bobjoe159 <pedroh1001@hotmail.com>
2022-06-13 17:14:04 -0300

    Adicionados alguns prompts

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let firstName = 'Pedro'
-let secondName = 'Feitosa'
-let yearBorn = '1997'
-let city = 'Belo Horizonte'
+alert('Sistema iniciado.')
+
+// let firstName = 'Pedro'
+// let secondName = 'Feitosa'
+// let yearBorn = '1997'
+// let city = 'Belo Horizonte'
+
+// Recebendo as varíaveis
+
+let firstName = prompt('Digite seu primeiro nome')
+let secondName = prompt('Digite seu sobrenome')
+let yearBorn = prompt('Digite seu ano de nascimento')
+let city = prompt('Digite sua cidade')
 
 let date = new Date()
 let actualAge = date.getFullYear()
 let age = actualAge - yearBorn
 
+// Imprimindo as varíaveis em alert e console
+
+alert(
+  `Meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
+)
+
 console.log(
-  `Olá, meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
+  `Meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
 )
