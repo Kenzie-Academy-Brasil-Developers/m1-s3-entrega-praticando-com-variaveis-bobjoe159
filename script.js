f71d9aa037499d75ab10ed7f81c4c1f7123c77b5
bobjoe159 <pedroh1001@hotmail.com>
2022-06-13 17:31:37 -0300

    Adicionado css[

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-alert('Sistema iniciado.')
-
 // let firstName = 'Pedro'
 // let secondName = 'Feitosa'
 // let yearBorn = '1997'
@@ -7,21 +5,24 @@ alert('Sistema iniciado.')
 
 // Recebendo as varíaveis
 
-let firstName = prompt('Digite seu primeiro nome')
-let secondName = prompt('Digite seu sobrenome')
-let yearBorn = prompt('Digite seu ano de nascimento')
-let city = prompt('Digite sua cidade')
+function startSystem() {
+  alert('Sistema iniciado.')
+  let firstName = prompt('Digite seu primeiro nome')
+  let secondName = prompt('Digite seu sobrenome')
+  let yearBorn = prompt('Digite seu ano de nascimento')
+  let city = prompt('Digite sua cidade')
 
-let date = new Date()
-let actualAge = date.getFullYear()
-let age = actualAge - yearBorn
+  let date = new Date()
+  let actualAge = date.getFullYear()
+  let age = actualAge - yearBorn
 
-// Imprimindo as varíaveis em alert e console
+  // Imprimindo as varíaveis em alert e console
 
-alert(
-  `Meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
-)
+  alert(
+    `Meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
+  )
 
-console.log(
-  `Meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
-)
+  console.log(
+    `Meu nome é ${firstName} ${secondName}, tenho ${age} anos e moro em ${city}.`
+  )
+}
